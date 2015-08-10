4d4508a9400c26a5930aa5576f39488dccdc3bf6
Alex Altieri & Toma Apostolov <student@epicodus.com>
2015-08-10 12:05:17 -0700

    Add initial files

---
+++ lib/parcels.rb
@@ -0,0 +1,7 @@
+class Parcels
+  define_method(:initialize) do |length, width, height|
+  end
+
+  define_method(:dimensions) do
+  end
+end
---
+++ spec/parcels_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('/parcels')
+
+describe('Parcels') do
+  describe('#dimensions') do
+    it()
+  end
+end
