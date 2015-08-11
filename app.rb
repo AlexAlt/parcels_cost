487ce626e3f67ca27975b8c79f73ef8e8bd5adaa
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-10 20:49:14 -0700

    Makes some changes that still dont really work but some of the code is better

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/parcels')
+require('./lib/parcels') 
+require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
@@ -8,11 +9,12 @@ get('/') do
 end
 
 get('/parcels') do
+binding.pry 
   @length = params.fetch('length').to_i
   @width = params.fetch('width').to_i
   @height = params.fetch('height').to_i
   @weight = params.fetch('weight').to_i
-  @next_day_checked = params.fetch('next_day_delivery')
+  @next_day_checked = params.fetch('next_day_delivery') 
   @standard_shipping = Parcels.new(@length, @width, @height, @weight).cost_to_ship
   @next_day_shipping = Parcels.new(@length, @width, @height, @weight).next_day_delivery
   erb(:product_results)
